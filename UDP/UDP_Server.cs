5886c29b3a59d2e8d31277442794b81fb9163e02
nik0rai <nikolay-sviridov@inbox.ru>
2023-01-13 00:17:04 +0300

    udp

---
+++ UDP/UDP_Server.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP_Server
+{
+    public class UDP_Server
+    {
+        static void Main(string[] args)
+        {
+            //LocalEndPoint: возвращает локальную точку (объект типа EndPoint), по которой запущен сокет и по которой он принимает данные
+            //RemoteEndPoint: возвращает адрес удаленного хоста, к которому подключен сокет (объект типа EndPoint)
+
+            //udp: bind, reciveFrom/reciveFromAsync (для отправки данных SendTo/SendToAsync)
+            //после завершения работы закрыть сокет: Close() или с самого начала обернуть в using(Socket socket = new Socket(...){}
+
+            //перменые 
+            int DATA = int.MinValue;
+            char CHAR = char.MinValue;
+            //server
+            var port = 1000;
+            var host = Dns.GetHostEntry("localhost");
+            var ipAddr = host.AddressList[0];
+            var localEndPoint = new IPEndPoint(ipAddr, port);
+
+            using (var listener = new Socket(ipAddr.AddressFamily, SocketType.Dgram, ProtocolType.Udp))
+            {
+                listener.ReceiveTimeout = 5000; //1 секунда таймаут на получение
+                listener.SendTimeout = 500;
+                listener.Bind(localEndPoint);
+
+                byte[] buffer = new byte[256];
+                Console.WriteLine($"Server is running and waiting for a connection on port: {port}");
+
+                EndPoint remoteIp = new IPEndPoint(ipAddr, port);
+
+                
+                var result = listener.ReceiveFrom(buffer, ref remoteIp);
+
+                DATA = BitConverter.ToInt32(buffer, 0);
+                CHAR = Convert.ToChar(Encoding.ASCII.GetString(buffer[(sizeof(int) - 1)..])[1]); //только так char получить
+                
+                Console.WriteLine($"Got: {DATA} and {CHAR}");
+                listener.SendTo(Encoding.ASCII.GetBytes("ACK"), remoteIp);
+                Console.WriteLine($"Sent message ACK to {remoteIp}");
+                listener.Shutdown(SocketShutdown.Both);
+            }
+        }
+    }
+}
---
+++ UDP/Udp_client.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TCP_client
+{
+    public static class Udp_client
+    {
+        static void Main(string[] args)
+        {
+            var port = 1000;
+            var host = Dns.GetHostEntry("localhost");
+            var ipAddr = host.AddressList[0];
+            var remoteEndPoint = new IPEndPoint(ipAddr, port);
+
+            //переменные
+            int data = 5;
+            char somesymbol = 'a';    
+            
+            using (var sender = new Socket(ipAddr.AddressFamily, SocketType.Dgram, ProtocolType.Udp))
+            {
+                var message = ToBytes(data, somesymbol);
+                int bytes = sender.SendTo(message, remoteEndPoint);
+                Console.WriteLine($"Sent message containing: {data} and {somesymbol}");
+
+                byte[] buffer = new byte[256];
+                var res = sender.Receive(buffer);
+                Console.WriteLine($"Got message: {Encoding.ASCII.GetString(buffer, 0, res)}");
+                sender.Shutdown(SocketShutdown.Both);
+            }
+        }
+
+        public static byte[] ToBytes(params object[] input)
+        {
+            List<int> individual_size = new();
+
+            int max_size = 0; //у нас может быть несколько параметров разных типов
+            for (int i = 0; i < input.Length; i++)
+            {
+                int jumper = Marshal.SizeOf(input[i]);
+                individual_size.Add(jumper);
+                max_size += jumper;
+            }
+
+            var result = new byte[max_size+1];
+
+            GCHandle gcHandle = new();
+            int index = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].GetType() == typeof(char)) input[i] = Convert.ToUInt16(input[i]); //ASCII support
+
+                gcHandle = GCHandle.Alloc(input[i], GCHandleType.Pinned);
+                Marshal.Copy(gcHandle.AddrOfPinnedObject(), result, index, individual_size[i]);
+                index += Marshal.SizeOf(individual_size[i]); //новый элемент будет находится с новго индекса
+            }
+            gcHandle.Free();
+            return result;
+        }
+    }
+}
